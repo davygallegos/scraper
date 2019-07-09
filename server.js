6d542263b96715034460ed79833745995c8ef5f2
davygallegos <david.gallegos@outlook.com>
2019-07-08 18:11:16 -0700

    heroku with mlab

--- server.js
+++ server.js
@@ -27,7 +27,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/xbox", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/xbox", { useNewUrlParser: true });
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/xbox";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
